b5d87adab7faa8e0fd1f9fc5a96dcdf8b5d905de
Matthew <msb2170@gmail.com>
2023-03-10 15:09:07 -0500

    style changes, commit to grid

--- client/src/components/MovieCard.js
+++ client/src/components/MovieCard.js
@@ -2,14 +2,14 @@
 export default function MovieCard({title, director, summary, genre, year, language, poster, handleDelete}) {
     return (
         <div className="card">
-            <h1>{title}</h1>
-            <img alt="movie poster" src={poster} />
-            <h3><em>{director}</em></h3>
-            <h4>{year}</h4>
-            <h4>{genre}</h4>
-            <h4>{language}</h4>
-            <p>{summary}</p>
-            <button onClick={handleDelete}>x</button>
+            <h1 className='card-title'>{title}</h1>
+            <button className="card-remove-btn" onClick={handleDelete}>x</button>
+            <img alt="movie poster" src={poster} className='card-img'/>
+            <h3 className="card-info"><em>{director}</em></h3>
+            <h4 className="card-info">{year}</h4>
+            <h4 className="card-info">{genre}</h4>
+            <h4 className="card-info">{language}</h4>
+            <p className="card-info">{summary}</p>
         </div>
     )
 }
